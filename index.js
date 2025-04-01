de3520b07c2562694e46dd70892bc460a85c22f9
apedreroes <apedreroes@MacBook-Air-de-Administrador.local>
2025-04-01 23:19:31 +0200

    Ciudades Versión 7

--- index.js
+++ index.js
@@ -5,17 +5,17 @@ const cors = require("cors");
 require("dotenv").config();
 const PORT = process.env.PORT || 3000;
 
-/*var corsOptions = { 
+var corsOptions = { 
     origin: 'http://api.openweathermap.org/*',
     optionsSuccessStatus: 200
-}*/
+}
 // Middlewares
-app.use(cors({
+/*app.use(cors({
     origin: '*', // Permite todas las solicitudes (no recomendado en producción)
     methods: 'GET,POST,PUT,DELETE',
     allowedHeaders: 'Content-Type,Authorization'
-  }));
-//app.use(cors(corsOptions));
+  }));*/
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
@@ -29,5 +29,5 @@ app.use(express.static('public'))
 
 // Iniciar servidor
 var server = app.listen(PORT, () => {
-    console.log(`mi app está escuchando en ${server.address().port}`)
+    console.log(`escuchando en ${server.address().port}`)
 })
